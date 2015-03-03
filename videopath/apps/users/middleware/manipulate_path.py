58cdd9e0785058bd6a23859f023a91bd3608c338
zevrector21 <zevrector21@gmail.com>
2015-03-03 13:35:05 +0100

    fixed some bugs

--- videopath/apps/users/middleware/manipulate_path.py
+++ videopath/apps/users/middleware/manipulate_path.py
@@ -9,14 +9,21 @@ class ManipulatePathMiddleware(object):
 
     def process_request(self, request):
 
-    	if "user/me" in request.path and "HTTP_AUTHORIZATION" in request.META:
-			token_string = request.META["HTTP_AUTHORIZATION"].replace("Token ", "").strip()
-			try:
-				user, token = token_util.authenticate_token(token_string)
-				if user:
-					url = "user/" + str(user.pk)
-					request.path = request.path.replace("user/me", url)
-					request.path_info = request.path_info.replace("user/me", url)
-			except exceptions.AuthenticationFailed:
-				pass
+    	if "user/me" in request.path:
+
+			# try to get user from request
+			user = request.user if request.user.is_authenticated() else None
+
+			if "HTTP_AUTHORIZATION" in request.META and not user:
+				token_string = request.META["HTTP_AUTHORIZATION"].replace("Token ", "").strip()
+
+				try:
+					user, token = token_util.authenticate_token(token_string)
+				except exceptions.AuthenticationFailed:
+					pass
+			
+			if user:
+				url = "user/" + str(user.pk)
+				request.path = request.path.replace("user/me", url)
+				request.path_info = request.path_info.replace("user/me", url)
 

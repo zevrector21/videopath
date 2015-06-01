7717de4e4da6f1ec33031cbbc938e2a304554443
zevrector21 <zevrector21@gmail.com>
2015-06-01 09:54:31 +0200

    added migration to player v3

---
+++ videopath/apps/videos/management/commands/migrate_to_player_3.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+from videopath.apps.videos.models import PlayerAppearance
+import shlex
+
+from videopath.apps.videos.models import Video
+
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+       	for v in Video.objects.all():
+       		v.player_version = '3'
+       		v.save()
+
+       		from videopath.apps.videos.util import video_export_util
+        	video_export_util.export_video(v)
+
+       		print "exported " + v.key
+
+	

c632bc01f9061204c454891deee266ec3929de5c
zevrector21 <zevrector21@gmail.com>
2015-06-18 18:25:03 +0200

    updated player staging urls

--- videopath/settings/env_staging.py
+++ videopath/settings/env_staging.py
@@ -10,3 +10,11 @@ STAGING = True
 import dj_database_url
 if dj_database_url.config():
     DATABASES['default'] = dj_database_url.config()
+
+
+#
+# Set dev player locations
+#
+AWS_PLAYER_BUCKET = "player-dev.videopath.com"
+PLAYER_SRC = '//player-dev.videopath.com/develop/'
+PLAYER_LOCATION = 'http://player-dev.videopath.com/'

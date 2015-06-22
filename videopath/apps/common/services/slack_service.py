96905ded92e7d338fc3b19f382b3761bf21c45aa
zevrector21 <zevrector21@gmail.com>
2015-06-22 16:55:12 +0200

    slack service now posts as robot user

--- videopath/apps/common/services/slack_service.py
+++ videopath/apps/common/services/slack_service.py
@@ -4,6 +4,6 @@ from django.conf import settings
 def notify(text):
 	try:
 		slack = Slacker(settings.SLACK_API_TOCKEN)
-		slack.chat.post_message('#product-activity', text)
+		slack.chat.post_message('#product-activity', text, as_user=True)
 	except:
 		pass

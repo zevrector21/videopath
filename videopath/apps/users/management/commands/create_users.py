af91a8df9d059a7a4e6eb44579b67bf48f6d16f2
zevrector21 <zevrector21@gmail.com>
2015-07-04 14:23:01 +0200

    added create users script

---
+++ videopath/apps/users/management/commands/create_users.py
@@ -0,0 +1,26 @@
+from django.core.management.base import BaseCommand
+from userena.models import UserenaSignup
+
+import sys
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+
+    	print sys.argv
+
+    	amount = 50
+
+    	while amount > 0:
+    		
+
+			username = "student-" + str(amount) + "@schiffers.info"
+			password = "videopath-"+ str(amount)
+
+			user = UserenaSignup.objects.create_user(username,
+			                             username,
+			                             password,
+			                             active=True, send_email=False)
+			amount-=1
+
+

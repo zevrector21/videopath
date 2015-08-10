5dfda3e9c4d3e35c964d424d6d5b1da2da5ddb44
zevrector21 <zevrector21@gmail.com>
2015-08-10 15:06:44 +0200

    fixed migration scfript

--- videopath/apps/videos/management/commands/migrate_to_player_4.py
+++ videopath/apps/videos/management/commands/migrate_to_player_4.py
@@ -10,7 +10,7 @@ from videopath.apps.videos.models import Video
 class Command(BaseCommand):
     def handle(self, *args, **options):
        	for v in Video.objects.all():
-       		v.player_version = ''
+       		v.player_version = '4'
        		v.save()
 
    		from videopath.apps.videos.util import video_export_util

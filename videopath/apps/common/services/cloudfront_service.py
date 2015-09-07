c9dd1176e01fe79c4f949951c6b788b78f635cb8
zevrector21 <zevrector21@gmail.com>
2015-09-07 13:02:18 +0200

    added fix for local cloudfront connection

--- videopath/apps/common/services/cloudfront_service.py
+++ videopath/apps/common/services/cloudfront_service.py
@@ -1,9 +1,12 @@
 from videopath.apps.common.services.cloudfront_custom.CloudFrontConnection import CloudFrontConnection
+from django.conf import settings
 
-connection = CloudFrontConnection()
+if not settings.LOCAL:
+	connection = CloudFrontConnection()
 
 def invalidate(distribution_id, path):
-	connection.create_invalidation_request(distribution_id, path)
+	if not settings.LOCAL:
+		connection.create_invalidation_request(distribution_id, path)
 	return True
 
 def check_access():

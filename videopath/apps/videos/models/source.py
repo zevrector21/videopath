a2ab9f7cd128e969f8a9fb7e41a7150e29620beb
David <david@videopath.com>
2015-11-05 15:38:39 +0100

    fixed bug in source model

--- videopath/apps/videos/models/source.py
+++ videopath/apps/videos/models/source.py
@@ -95,7 +95,7 @@ class Source(VideopathBaseModel):
             return  False, "Project is already transcoded for iPhone."
         if self.duration > 600:
             return False, "Currently only projects shorter than 10 Minutes can be transcoded."
-        if self.service not in [Source.SERVICE_YOUTUBE or Source.SERVICE_YOUTUBE]:
+        if self.service not in [Source.SERVICE_YOUTUBE, Source.SERVICE_YOUTUBE]:
             return False, "Currently only youtube projects can be transcoded."
 
         service_connection = service_provider.get_service('services')

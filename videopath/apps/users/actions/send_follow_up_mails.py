6fc17798afaf4c3388d86a529c6d252ae44b7594
David Scharf <david@videopath.com>
2015-11-24 16:00:52 +0100

    follow up emails now cut of at about 2 months

--- videopath/apps/users/actions/send_follow_up_mails.py
+++ videopath/apps/users/actions/send_follow_up_mails.py
@@ -7,6 +7,7 @@ from videopath.apps.users.models import AutomatedMail
 
 TREE_WEEKS = 21
 SIX_WEEKS = 42	
+TWO_MONTHS = 70
 DEFAULT_MAX_MAILS = 10
 
 def run(max_mails=DEFAULT_MAX_MAILS):
@@ -15,10 +16,11 @@ def run(max_mails=DEFAULT_MAX_MAILS):
 
 
 def _send_three_weeks_follow_up(max_mails):
-	thresh = datetime.now() - timedelta(days=TREE_WEEKS)
+	three_weeks_ago = datetime.now() - timedelta(days=TREE_WEEKS)
+	two_months_ago = datetime.now() - timedelta(days=TWO_MONTHS)
 
 	# all users which have received mail 21 and have not been seen for longer than 3 weeks
-	users = User.objects.filter(activity__last_seen__lte=thresh)
+	users = User.objects.filter(activity__last_seen__lte=three_weeks_ago, date_joined__gte=two_months_ago)
 	users = users.exclude(automated_mails__mailtype=AutomatedMail.TYPE_FOLLOW_UP_21)
 
 	count = 0
@@ -29,16 +31,17 @@ def _send_three_weeks_follow_up(max_mails):
 		if count > max_mails: break
 
 def _send_six_weeks_follow_up(max_mails):
-	thresh = datetime.now() - timedelta(days=SIX_WEEKS)
+	six_weeks_ago = datetime.now() - timedelta(days=SIX_WEEKS)
+	two_months_ago = datetime.now() - timedelta(days=TWO_MONTHS)
 
 	# all users which have received mail 21 and have not been seen for longer than 6 weeks
-	users = User.objects.filter(activity__last_seen__lte=thresh, automated_mails__mailtype = AutomatedMail.TYPE_FOLLOW_UP_21)
+	users = User.objects.filter(activity__last_seen__lte=six_weeks_ago,  date_joined__gte=two_months_ago, automated_mails__mailtype = AutomatedMail.TYPE_FOLLOW_UP_21)
 	users = users.exclude(automated_mails__mailtype=AutomatedMail.TYPE_FOLLOW_UP_42)
 
 	count = 0
 	for u in users:
-		thresh_three_weeks = datetime.now() - timedelta(days=TREE_WEEKS)
-		if u.automated_mails.filter(mailtype=AutomatedMail.TYPE_FOLLOW_UP_21, created__gte=thresh_three_weeks).count():
+		three_weeks_ago = datetime.now() - timedelta(days=TREE_WEEKS)
+		if u.automated_mails.filter(mailtype=AutomatedMail.TYPE_FOLLOW_UP_21, created__gte=three_weeks_ago).count():
 			continue
 		# send
 		AutomatedMail.objects.create(user=u, mailtype = AutomatedMail.TYPE_FOLLOW_UP_42)

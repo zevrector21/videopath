8d3a7173916d3420c6ac45be78e9cd6ad7f74650
David Scharf <david@videopath.com>
2015-11-24 14:47:50 +0100

    changed users export command

--- videopath/apps/users/actions/export_users_to_csv.py
+++ videopath/apps/users/actions/export_users_to_csv.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 
 def run():
 
-	for u in User.objects.all().order_by('date_joined'):
+	for u in User.objects.all().order_by('-date_joined'):
 		print u.email + ',' + str(u.date_joined.date())
 
     	

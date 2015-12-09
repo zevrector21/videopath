9ab44b74b750905cf2f48a050a193cf6bf3c1761
David Scharf <david@videopath.com>
2015-12-09 14:27:04 +0100

    fixed tests in integrations app

--- videopath/apps/integrations/tests/endpoints/integrations.py
+++ videopath/apps/integrations/tests/endpoints/integrations.py
@@ -2,7 +2,7 @@ from videopath.apps.videos.tests.endpoints.endpoints_base import EndpointsBaseTe
 
 from videopath.apps.integrations.models import Integration
 
-INTEGRATION_URL = '/v1/integration/'
+INTEGRATION_URL = '/v1/team/{0}/integration/'
 
 # Uses the standard django frame testing client
 class TestCase(EndpointsBaseTestCase):
@@ -11,7 +11,7 @@ class TestCase(EndpointsBaseTestCase):
         self.setup_users_and_clients()
 
         # should show a list of integrations
-        response = self.client_user1.get_json(INTEGRATION_URL)
+        response = self.client_user1.get_json(INTEGRATION_URL.format(self.user1.default_team.pk))
         self.assertTrue(response.data.get('count') >= 1)
         self.assertEqual(response.status_code, 200)
 
@@ -19,18 +19,18 @@ class TestCase(EndpointsBaseTestCase):
 		self.setup_users_and_clients()
 
 		# unconfigured integration
-		response = self.client_user1.get_json(INTEGRATION_URL + 'mailchimp/')
+		response = self.client_user1.get_json(INTEGRATION_URL.format(self.user1.default_team.pk) + 'mailchimp/')
 		self.assertEqual(response.data.get('id'), 'mailchimp')
 		self.assertEqual(response.data.get('configured'), False)
 		self.assertEqual(response.status_code, 200)
 
 		# create integration and see if the return is correct
 		Integration.objects.create(team=self.user1.default_team, service='mailchimp')
-		response = self.client_user1.get_json(INTEGRATION_URL + 'mailchimp/')
+		response = self.client_user1.get_json(INTEGRATION_URL.format(self.user1.default_team.pk) + 'mailchimp/')
 		self.assertEqual(response.data.get('id'), 'mailchimp')
 		self.assertEqual(response.data.get('configured'), True)
 
 		# delete integration
-		self.client_user1.delete_json(INTEGRATION_URL + 'mailchimp/')
-		response = self.client_user1.get_json(INTEGRATION_URL + 'mailchimp/')
+		self.client_user1.delete_json(INTEGRATION_URL.format(self.user1.default_team.pk) + 'mailchimp/')
+		response = self.client_user1.get_json(INTEGRATION_URL.format(self.user1.default_team.pk) + 'mailchimp/')
 		self.assertEqual(response.data.get('configured'), False)

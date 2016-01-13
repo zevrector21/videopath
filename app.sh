c79729c6a0f97b86655802187f4e3700be07598b
David Scharf <david@videopath.com>
2016-01-13 16:52:18 +0100

    another small change to gunicorn

--- app.sh
+++ app.sh
@@ -7,7 +7,7 @@ if [ "$1" == "setup" ]; then
 	python manage.py check_permissions
 elif [ "$1" == "run" ]; then
     echo "Starting app"
-    gunicorn -b 127.0.0.1:5000 --certfile certs/server.crt --keyfile certs/server.key videopath.wsgi 
+    gunicorn -b 127.0.0.1:5000  --access-logfile - --certfile certs/server.crt --keyfile certs/server.key --do-handshake-on-connect videopath.wsgi 
 elif [ "$1" == "run_worker" ]; then
     # start redis server in background
 	redis-server & 

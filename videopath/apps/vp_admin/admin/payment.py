d754c2a62012f03d27281c2ba7cdc59037e39a37
zevrector21 <zevrector21@gmail.com>
2016-03-01 11:17:57 +0100

    updated payment admin

--- videopath/apps/vp_admin/admin/payment.py
+++ videopath/apps/vp_admin/admin/payment.py
@@ -9,7 +9,8 @@ from ..models import  Payment
 # Payents admin
 #
 class PaymentAdmin(VideopathModelAdmin):
-	list_display = ('number', 'recipient', 'total', 'created', 'paid','provider', 'link')
+
+	list_display = ('number', 'recipient', 'total', 'created', 'paid','provider', 'link', 'current_plan')
 	ordering = ('-created',)
 	search_fields = ['number', 'user__email', 'user__username']
 	list_filter = ['paid', 'created']
@@ -17,14 +18,37 @@ class PaymentAdmin(VideopathModelAdmin):
 
 	def recipient(self, obj):
 		return obj.user.email
+	recipient.admin_order_field = 'user__email'
+
 
 	def total(self, obj):
 		return '{0} {1}'.format(obj.amount_due/100, obj.currency)
+	total.admin_order_field = 'amount_due'
+
 
 	def link(self, payment):
 		url = payment_export_util.url_for_payment(payment) 
 		return "<a href = '%s'>View</a>" % url
 	link.allow_tags = True
+
+	#
+	# List display
+	#
+	def current_plan(self, obj):
+		try:
+			plan = obj.user.subscription.plan
+		except:
+			return 'free-free'
+		status = ''
+		try:
+			status = 'switches to {0} on {1}'.format(obj.user.pending_subscription.plan, obj.user.subscription.current_period_end)
+		except: 	
+			if obj.user.subscription.current_period_end:
+				status = 'renews {0}'.format(obj.user.subscription.current_period_end)
+
+		return "{0}<br />{1}".format(plan, status)
+	current_plan.admin_order_field = 'user__subscription__plan'
+	current_plan.allow_tags = True
 
 
 	#

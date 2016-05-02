5777ec4de41d69068e9fe3399ef9e9d6b7c32728
zevrector21 <zevrector21@gmail.com>
2016-05-02 11:57:30 +0200

    updated player migration script

--- videopath/apps/videos/management/commands/migrate_to_player_6.py
+++ videopath/apps/videos/management/commands/migrate_to_player_6.py
@@ -6,11 +6,23 @@ import shlex
 from videopath.apps.videos.models import Video
 
 
+accs = [
+  'max@videopath.co',
+  'max@videopath.com',
+  'maxvideopath@gmail.com',
+  'maximilianmealing@gmail.com',
+  'anna@videopath.com',
+  'anna2rose@gmail.com',
+  'desiree@videopath.com',
+  'adillon_88@hotmail.com',
+  'louisa.denison@gmail.com',
+  'tim@videopath.com'
+]
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
     	count = 0
-       	for v in Video.objects.filter(team__owner__email='info@videopath.com'):
+       	for v in Video.objects.filter(team__owner__email__in=accs):
        		count += 1
        		print count
        		v.player_version = '6'

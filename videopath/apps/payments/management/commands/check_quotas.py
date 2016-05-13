66c45ac4683269113556f1e90f0560575f02dc93
David Scharf <davidscharf@Davids-iMac.local>
2016-05-13 15:55:29 +0200

    removed unneeded files and code

--- videopath/apps/payments/management/commands/check_quotas.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-
-from videopath.apps.payments import quota_manager
-
-class Command(BaseCommand):
-
-    def handle(self, *args, **options):
-        quota_manager.check_quotas()
--- videopath/apps/payments/management/commands/process_payments.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-
-from videopath.apps.payments.util import payment_util
-
-class Command(BaseCommand):
-
-    def handle(self, *args, **options):
-        payment_util.process_payments()

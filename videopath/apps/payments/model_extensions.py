502688f0027a1723a86e5a776fc484cb9062754c
zevrector21 <zevrector21@gmail.com>
2016-05-13 18:41:18 +0200

    fixed subscription bug

--- videopath/apps/payments/model_extensions.py
+++ videopath/apps/payments/model_extensions.py
@@ -5,10 +5,11 @@ from videopath.apps.payments.util import subscription_util
 # Users User
 #
 def subscribe_to_plan(self, plan=None, coupon=None):
-	subscription_util.subscribe_user(self,plan,coupon)
+	return subscription_util.subscribe_user(self,plan,coupon)
 User.add_to_class('subscribe_to_plan', subscribe_to_plan)
 
 
 def unsubscribe_from_plan(self):
-	subscription_util.unsubscribe_user(self)
+	return subscription_util.unsubscribe_user(self)
 User.add_to_class('unsubscribe_from_plan', unsubscribe_from_plan)
+

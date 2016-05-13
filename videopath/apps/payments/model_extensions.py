64a111c71010fd1c94997e3cbbb189aa0ba33482
zevrector21 <zevrector21@gmail.com>
2016-05-13 19:31:32 +0200

    small change

--- videopath/apps/payments/model_extensions.py
+++ videopath/apps/payments/model_extensions.py
@@ -6,10 +6,10 @@ from videopath.apps.payments.util import subscription_util
 #
 def subscribe_to_plan(self, plan=None, coupon=None):
 	return subscription_util.subscribe_user(self,plan,coupon)
-User.add_to_class('subscribe_to_plan', subscribe_to_plan)
+User.subscribe_to_plan = classmethod(subscribe_to_plan)
 
 
 def unsubscribe_from_plan(self):
 	return subscription_util.unsubscribe_user(self)
-User.add_to_class('unsubscribe_from_plan', unsubscribe_from_plan)
+User.unsubscribe_from_plan = classmethod(unsubscribe_from_plan)
 
